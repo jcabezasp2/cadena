60ac4c372a8c73c940d1e434f73fb697c9b27e1d
jcabe <jcabe@DESKTOP-75RMB14>
2022-03-23 22:34:32 +0100

    Finalizado

--- src/cadena/CadenaTest.java
+++ src/cadena/CadenaTest.java
@@ -12,7 +12,7 @@ class CadenaTest {
 	String cadena2 = "bbbbb";
 	String cadena3 = "aaaaa";
 	String cadena4 = "";
-	String cadena5 = "aña";
+	String cadena5 = "Peña";
 	String cadena6 = "1551";
 	String cadena7 = "àbcd";
 	Cadena cadena = new Cadena();
@@ -32,7 +32,7 @@ class CadenaTest {
 	@Test
 	void testLongitudConN() {
 	
-		Assertions.assertEquals(3, cadena.longitud(cadena5));
+		Assertions.assertEquals(4, cadena.longitud(cadena5));
 	}
 
 	@Test
@@ -44,24 +44,73 @@ class CadenaTest {
 	@Test
 	void testVocalesBasico() {
 	
-		Assertions.assertEquals(1, cadena.longitud(cadena1));
+		Assertions.assertEquals(1, cadena.vocales(cadena1));
 	}
 	
 	@Test
 	void testVocalesCadenaVacia() {
 	
-		Assertions.assertEquals(0, cadena.longitud(cadena4));
+		Assertions.assertEquals(0, cadena.vocales(cadena4));
 	}
 	
 	@Test
 	void testVocalesConN() {
 	
-		Assertions.assertEquals(2, cadena.longitud(cadena5));
+		Assertions.assertEquals(2, cadena.vocales(cadena5));
 	}
 	
 	@Test
 	void testVocalesConAcento() {
 	
-		Assertions.assertEquals(2, cadena.longitud(cadena7));
+		Assertions.assertEquals(1, cadena.vocales(cadena7));
+	}
+	
+
+	@Test
+	void testInvertirBasico() {
+	
+		Assertions.assertEquals("dcba", cadena.invertir(cadena1));
+	}
+	
+	@Test
+	void testInvertirCapicua() {
+	
+		Assertions.assertEquals("1551", cadena.invertir(cadena6));
+	}
+	
+	@Test
+	void testInvertirCadenaVacia() {
+	
+		Assertions.assertEquals("", cadena.invertir(cadena4));
+	}
+	
+	@Test
+	void testinvertirConN() {
+	
+		Assertions.assertEquals("añeP", cadena.invertir(cadena5));
+	}
+	
+
+	@Test
+	void testContarLetraBasico() {
+	
+		Assertions.assertEquals(1, cadena.contarLetra(cadena1, 'c'));
+	}
+	
+	@Test
+	void testContarLetraCadenaVacia() {
+	
+		Assertions.assertEquals(0, cadena.contarLetra(cadena4, 'a'));
+	}
+	@Test
+	void testContarLetraConN() {
+	
+		Assertions.assertEquals(1, cadena.contarLetra(cadena5, 'ñ'));
+	}
+	
+	@Test
+	void testContarLetraConAcento() {
+	
+		Assertions.assertEquals(1, cadena.contarLetra(cadena7, 'à'));
 	}
 }
